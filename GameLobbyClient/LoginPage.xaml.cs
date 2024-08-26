1d2375d82b7d987d9c44d9076aef8da3097f4bd2
Samuel Eaves <19137858@student.curtin.edu.au>
2024-08-26 16:13:06 +0800

    Added all GUI files from previous attempt. Uses a MainWindow to hold a frame and changes pages as required to different views. Currently uses redundant code for testing purposes.

---
+++ GameLobbyClient/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLobbyClient
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        /* 
+         * Takes username from loginbox and passes to MainLobbyPage
+         * CHANGE when classes are implemented
+         */
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = LoginBox.Text;
+            MainLobbyPage lobbyPage = new MainLobbyPage(username);
+            NavigationService.Navigate(lobbyPage);
+        }
+    }
+}
---
+++ GameLobbyClient/MainLobbyPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLobbyClient
+{
+    /// <summary>
+    /// Interaction logic for MainLobbyPage.xaml
+    /// </summary>
+    public partial class MainLobbyPage : Page
+    {
+        //Counts total lobbies to max of 5
+        private int lobbyCount = 0;
+        //String username temp which may be replaced by class use
+        private string userName;
+        public MainLobbyPage(string username)
+        {
+            InitializeComponent();
+            userName = username;
+
+            //Redundant lines for testing
+            UsernameBlock.Text = userName;
+            UsernameBlock.Visibility = Visibility.Visible;
+        }
+
+        /* 
+         * Create lobby button
+         * Checks lobbyCount to ensure max hasn't been hit
+         * Checks for non null or empty input
+         * Switchcase handles which button to use/name
+         * One final check to ensure lobbyButton isn't null before setting
+         */
+        private void CreateLobbyButton_Click(object sender, RoutedEventArgs e)
+        {
+            string lobbyName = LobbyNameBox.Text;
+            if (lobbyCount >= 5)
+            {
+                MessageBox.Show("Maximum number of lobbies reached.");
+            }
+            else if (string.IsNullOrEmpty(lobbyName))
+            {
+                MessageBox.Show("Please enter a lobby name.");
+            }
+
+            //Handles making lobbies visible based on count
+            else
+            {
+                lobbyCount++;
+                Button lobbyButton = null;
+
+                switch (lobbyCount)
+                {
+                    case 1:
+                        lobbyButton = LobbyButtonOne;
+                        break;
+
+                    case 2:
+                        lobbyButton = LobbyButtonTwo;
+                        break;
+
+                    case 3:
+                        lobbyButton = LobbyButtonThree;
+                        break;
+
+                    case 4:
+                        lobbyButton = LobbyButtonFour;
+                        break;
+
+                    case 5:
+                        lobbyButton = LobbyButtonFive;
+                        break;
+                }
+
+                //Sets name and makes visible
+                if (lobbyButton != null)
+                {
+                    lobbyButton.Content = lobbyName;
+                    lobbyButton.Visibility = Visibility.Visible;
+                }
+
+                //Clears LobbyNameBox of previous text
+                LobbyNameBox.Clear();
+            }
+        }
+
+        /*
+         * Each button click is represented by what lobby it is.
+         * It takes the user-named lobby and passes it to a ChatLobbyPage
+         * Which then we navigate to through NavigationService
+         */
+        private void LobbyButtonOne_Click(object sender, RoutedEventArgs e)
+        {
+            ChatLobbyPage lobbyOne = new ChatLobbyPage(LobbyButtonOne.Content.ToString());
+            NavigationService.Navigate(lobbyOne);
+        }
+
+        private void LobbyButtonTwo_Click(object sender, RoutedEventArgs e)
+        {
+            ChatLobbyPage lobbyTwo = new ChatLobbyPage(LobbyButtonTwo.Content.ToString());
+            NavigationService.Navigate(lobbyTwo);
+        }
+
+        private void LobbyButtonThree_Click(object sender, RoutedEventArgs e)
+        {
+            ChatLobbyPage lobbyThree = new ChatLobbyPage(LobbyButtonThree.Content.ToString());
+            NavigationService.Navigate(lobbyThree);
+        }
+
+        private void LobbyButtonFour_Click(object sender, RoutedEventArgs e)
+        {
+            ChatLobbyPage lobbyFour = new ChatLobbyPage(LobbyButtonFour.Content.ToString());
+            NavigationService.Navigate(lobbyFour);
+        }
+
+        private void LobbyButtonFive_Click(object sender, RoutedEventArgs e)
+        {
+            ChatLobbyPage lobbyFive = new ChatLobbyPage(LobbyButtonFive.Content.ToString());
+            NavigationService.Navigate(lobbyFive);
+        }
+    }
+}

d6ae03924ee8c466d48fc93dac8b84cc3548b2ab
Marcus Dickson <124563538+mdickson05@users.noreply.github.com>
2024-08-26 15:34:45 +0800

    FEAT: Added Users class

---
+++ Users/User.cs
@@ -0,0 +1,17 @@
+﻿using Rooms;
+using System.Collections.Generic;
+
+namespace Users
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<Room> Rooms { get; set; }
+
+        public User(string username)
+        {
+            this.Username = username;
+            this.Rooms = new List<Room>(); // no room yet
+        }
+    }
+}

34fd036ebf9d575da6d05e03ee584fa28e76fcbf
Marcus Dickson <21549225@student.curtin.edu.au>
2024-08-26 15:23:36 +0800

    FEAT: Added Rooms project to the solution

---
+++ Rooms/Room.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Rooms
+{
+    public class Room
+    {
+        public string Roomname { get; set; } // includes the name of the room
+        public List<string> Users { get; set; } // as well as all users in the room
+
+        public Room(string roomName)
+        {
+            this.Roomname = roomName;
+            this.Users = new List<string>();
+        }
+    }
+}
---
+++ Rooms/RoomManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Rooms
+{
+    public class RoomManager
+    {
+        public Dictionary<string, Room> RoomsList { get; set; } // includes a list of all rooms in the Server
+        public RoomManager()
+        {
+            this.RoomsList = new Dictionary<string, Room>();
+        }
+
+
+        // logic for creating new room
+        // conditions: room cannot already exist
+        // return boolean for extra error handling abilities
+        public bool CreateNewRoom(string roomName)
+        {
+            if (!RoomsList.ContainsKey(roomName))
+            {
+                Room newRoom = new Room(roomName);
+                RoomsList.Add(roomName, newRoom);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // logic for deleting room
+        // conditions: no users can be in a room for it to be deleted.
+        // return boolean for extra error handling abilities
+        public bool DeleteRoom(string roomName)
+        {
+            Room toDelete = RoomsList[roomName];
+
+            if (toDelete != null && toDelete.Users.Count == 0)
+            {
+                RoomsList.Remove(roomName);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

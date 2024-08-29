58d6786ad4dfbf209b05d49fde2b1fb4a1e21802
sam <samuel.eaves@student.curtin.edu.au>
2024-08-29 09:35:38 +0800

    Added logout buttons to all user pages. Added private message page and implemented logic and test code.

--- GameLobbyClient/PrivateMessagePage.xaml.cs
+++ GameLobbyClient/PrivateMessagePage.xaml.cs
@@ -17,42 +17,34 @@ using System.Windows.Shapes;
 namespace GameLobbyClient
 {
     /// <summary>
-    /// Interaction logic for ChatLobbyPage.xaml
+    /// Interaction logic for PrivateMessagePage.xaml
     /// </summary>
-    public partial class ChatLobbyPage : Page
+    public partial class PrivateMessagePage : Page
     {
-        //Observable collections allows visual updating for both lists
         public ObservableCollection<string> ChatMessages { get; set; }
         public ObservableCollection<string> testUsers { get; set; }
 
         /*
-         * Creates instances of both lists and adds some test strings
-         * 
-         * DataContext is how "binding" works inside the xml
-         * We bind the list to a specific object/thing
-         * This will be replaced later when we incorporate classes and instances
-         * 
-         * Might implement backout button for ChatLobbyPage to return to MainLobbyPage
-         *  for lobby reselection
-         * 
+         * Might take in person object
+         * Will make similar to ChatLobbyPage for now
+         * Might implement a back button to return to ChatLobbyPage
+         *  that user was previously in or MainLobbyPage to re-enter
          */
-        public ChatLobbyPage(string lobbyName)
+        public PrivateMessagePage(string userName)
         {
             InitializeComponent();
-            DataContext = this; // Review above comment block
-            LobbyNameBlock.Text = lobbyName; //Sets lobby name
+            PrivateNameBlock.Text = userName;
+            DataContext = this; // Review in ChatLobbyPage for details
             ChatMessages = new ObservableCollection<string>(); //Testing chat messages
             testUsers = new ObservableCollection<string>(); //Testing users
 
             testUsers.Add("Joe");
             testUsers.Add("Bob");
-            testUsers.Add("Garry");
-            testUsers.Add("Beans");
 
             ChatMessages.Add("Joe: Hello!");
-            ChatMessages.Add("Bob: Hi there!");
-            ChatMessages.Add("Garry: Good morning!");
+            ChatMessages.Add("Joe: Finished the assignment yet?");
         }
+
 
         /*
          * User send message button, takes input from box and saves into collection
@@ -66,13 +58,13 @@ namespace GameLobbyClient
 
         /*
          * Copied above function just to test SendMessageButton_Click
+         * Make popout warning to say already in private message
          */
         private void PrivateMessageButton_Click(object sender, RoutedEventArgs e)
         {
-            Button tempButton = sender as Button;
-            string userName = tempButton.Content.ToString();
-            PrivateMessagePage privateMessagePage = new PrivateMessagePage(userName);
-            NavigationService.Navigate(privateMessagePage);
+            string message = $"{testUsers[0]}: {UserInputBox.Text}";
+            ChatMessages.Add(message);
+            UserInputBox.Clear();
         }
 
         /*
